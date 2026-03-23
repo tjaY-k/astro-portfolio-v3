fed7019eaf04ed6876782d1d9a0e398752062ef6
tjaY-k <kslaptev@gmail.com>
2026-03-23 22:45:49 +0400

    Comment out NewsletterForm export in index.ts
    Comment out the export of NewsletterForm component.

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,7 +1,7 @@
 export { default as ArticleCard } from './ArticleCard.astro';
 export { default as BackgroundArt } from './BackgroundArt.astro';
 export { default as LanguageSwitcher } from './LanguageSwitcher.astro';
-export { default as NewsletterForm } from './NewsletterForm.astro';
+// export { default as NewsletterForm } from './NewsletterForm.astro';
 export { default as ShareModal } from './ShareModal.astro';
 export { default as SocialLink } from './SocialLink.astro';
 export { default as StickySocials } from './StickySocials.astro';
